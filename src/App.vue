c1199a16615bfca433c3cbacb9de8b1ee6e043d8
lxk2 <737697366@qq.com>
2019-12-13 19:13:25 +0800

    abc

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,16 @@
   </div>
 </template>
 
+<script>
+export default {
+  created () {
+    if (!/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+      window.location.replace('http://cs.thelatest.cn/')
+    }
+  }
+}
+</script>
+
 <style lang="scss">
 @import '~@/assets/style/comm.scss';
 </style>
